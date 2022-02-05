1b0f64c27f64377662e0cf29cc471144582b47ba
wrafanunes <wrsnunes@gmail.com>
2022-02-05 13:15:42 -0300

    definindo uma nova página inicial

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+# definir uma página como inicial
+root 'home#index'
   # Defines the root path route ("/")
   # root "articles#index"
 end
