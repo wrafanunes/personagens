5b1a64b4835754c2cf1d29fcbf318bd569950ecd
wrafanunes <wrsnunes@gmail.com>
2022-02-05 18:47:37 -0300

    autenticação com a gem devise v4.8.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :characters
+  resources :users
   get 'home/index'
   get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_05_163648) do
+ActiveRecord::Schema.define(version: 2022_02_05_205424) do
 
   create_table "characters", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,18 @@ ActiveRecord::Schema.define(version: 2022_02_05_163648) do
     t.string "origin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at", precision: 6
+    t.datetime "remember_created_at", precision: 6
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
